75d6a10fd8de4fda66e652c428aa0b0d8b6ad5e5
remi telenczak <remi.telenczak@epitech.eu>
2023-01-22 17:02:24 +0100

    ok

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import useBricks from 'usebricks';
+import './App.css';
+
+
+function TwitterHorizontal() {
+  var { user, widget, configMode, props } = useBricks();
+  return (
+    <div className="App" style={{
+      backgroundColor: "#00ACEE",
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "start",
+      alignItems: "center",
+
+    }}>
+      <input type="text" className='inputHorizontal' placeholder="@username" />
+      <img src="/twitter.png" />
+    </div>
+  )
+}
+
+function TwitterSquare() {
+  var { user, widget, configMode, props } = useBricks();
+  return (
+    <div className="App" style={{
+      backgroundColor: "#00ACEE",
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "start",
+      alignItems: "start",
+      flexDirection: "column",
+
+    }}>
+      <input type="text" className='inputSquare' placeholder="@username" />
+      <p>50 following</p>
+      <p>43 followees</p>
+      <img src="/twitter.png" />
+    </div>
+  )
+}
+function App() {
+  var { user, widget, configMode, props } = useBricks();
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  if (height < 100) {
+    return <TwitterHorizontal />
+  }
+  return <TwitterSquare />
+}
+
+export default App
